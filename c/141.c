d3f3e53d8e6b78bea801e67c4129f20b7fdcdaea
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-06-15 17:52:51 -0300

    Teste aula - 141

---
+++ c/141.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+    double resultado;
+
+    a = 5;
+    b = 7;
+    resultado = (double) b / a;
+    printf("%.2lf\n", resultado);
+
+    return 0;
+}
