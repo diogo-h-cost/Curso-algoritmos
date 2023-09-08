79498ce899227ed85db2752b7ff8bc8c3cb7d9a6
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-09-07 22:33:14 -0300

    159 - Media_idades

---
+++ c/media_idades.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int idade, cont, soma;
+    double media;
+
+    printf("Digite as idades:\n");
+    scanf("%d", &idade);
+
+    soma = 0;
+    cont = 0;
+
+    while (idade >= 0) {
+        soma += idade;
+        cont += 1;
+        scanf("%d", &idade);
+    }
+
+    if (cont == 0) {
+        printf("Impossivel Calcular\n");
+    }
+    else {
+        media = (double) soma / cont;
+        printf("Media: %.2lf\n", media);
+    }
+
+    return 0;
+}
