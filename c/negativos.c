f56b451971e06ec7f864e3581189dddc15eae8c5
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-09-08 17:03:53 -0300

    162 - Negativos

---
+++ c/negativos.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Quantos numeros voce vai digitar? ");
+    scanf("%d", &n);
+
+    int vet[n];
+
+    for (int i = 0; i < n; i++) {
+        printf("Digite um numero: ");
+        scanf("%d", &vet[i]);
+    }
+
+    printf("\nNumeros Negativos:\n");
+    for (int i = 0; i < n; i++) {
+        if (vet[i] < 0) {
+            printf("%d\n", vet[i]);
+        }
+    }
+
+    return 0;
+}
